2ca2ab53aaef27d7bf0390c2fa0d2d39411a7599
aenonTEN <87352025+aenonTEN@users.noreply.github.com>
2022-10-07 12:55:41 +0530

    Add files via upload

---
+++ src/app/components/main-container/chat-area/chat-area.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-chat-area',
+  templateUrl: './chat-area.component.html',
+  styleUrls: ['./chat-area.component.scss']
+})
+export class ChatAreaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  formSubmit(form: NgForm): void {
+    console.log('');
+  }
+
+}
---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule} from '@angular/material/icon';
+
+const modules:MatIconModule[] = [
+  MatIconModule
+];
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    modules
+  ],
+  exports: [
+    modules
+
+  ]
+}
+)
+export class MaterialModule { }
